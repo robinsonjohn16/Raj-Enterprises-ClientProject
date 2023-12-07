9ee4aa2aba0c36d035c1e305c1af73e2b056018e
Nadar Robinson <robinsonnadar16@gmail.com>
2023-12-07 22:50:13 +0530

    "locomotive and preloader added"

--- javaScripts/scripts.js
+++ javaScripts/scripts.js
@@ -28,3 +28,36 @@ toggleBtn.onclick = function () {
   const isOpen = dropDowm.classList.contains("open");
   toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
 };
+
+// // Gsap and Scroll Trigger Codes
+// let timeLine = gsap.timeline();
+// timeLine.from("header h1", {
+//   y: -30,
+//   duration: 0.8,
+//   delay: 0.5,
+// });
+
+// gsap.from("main .product", {
+//   y: 30,
+//   scale: 0.98,
+//   opacity: 0,
+//   duration: 1,
+//   stager: 0.9,
+//   scrollTrigger: {
+//     trigger: "main .product",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 80%",
+//   },
+// });
+
+//* Locomotive Scroll
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("body"),
+  smooth: true,
+});
+
+let loader = document.querySelector("#preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
